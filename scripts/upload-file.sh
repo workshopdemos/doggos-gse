27b731ce859ae9d4b903d8507d69722c7189ceba
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-05-09 12:41:54 +0200

    Decoupling the scripts and readme updates

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 source ./basescript.sh
-
-printf "\nDeploying the input file to the Mainframe\n\n"
-
 printf "Enter username: "
 read hlq
+printf "\nDeploying the input file to the Mainframe\n\n"
 tempDirectoryName="FILES_TEMP";
 DOGGOS_INPUT="DOGGOS.INPUT";
 DOGGOS_INPUT_NEW="DOGGOS.INPUT.NEW";
 DOGGOS_RUNAPP="DOGGOS.RUNAPP"
 storageClass="CAEDS6";
 upload "$DOGGOS_INPUT" files/DOGGOS.INPUT DS N
-printf "Datasets successfully deployed to the Z/OS system\n\n";
 printf "\n###########################################################"
-printf "\n#### Datasets successfully deployed to the Z/OS system ####"
+printf "\n# Input dataset successfully deployed to the Z/OS system #"
 printf "\n###########################################################\n"
