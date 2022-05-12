57e2cb4663b9549b80d2caf364dbb27640a58cfd
Nikolai Imbirev <nikolay.imbirev@broadcom.com>
2022-05-12 12:53:29 +0200

    install prettier
    Signed-off-by: Nikolai Imbirev <nikolay.imbirev@broadcom.com>

--- docs/types.md
+++ docs/types.md
@@ -1,13 +1,13 @@
 # **Endevor types and processor groups**
+
 ### Branch **main**
-**System GSECONF**  
-  
-| Type | Processor Group Name | Description   |  
-| :----- | :----- | :----- |  
-| COBOL |  | COBOL SOURCE CODE |  
-|  | \*NOPROC\* | NO PROCESSORS ASSIGNED |  
-|  | CLENBL __`(Default)`__ | COBOL/LE COMPILE AND LINK, LISTING IS STORED |  
-| COPY |  | COBOL COPYBOOK |  
-|  | \*NOPROC\* | NO PROCESSORS ASSIGNED |  
 
+**System GSECONF**
 
+| Type  | Processor Group Name   | Description                                  |
+| :---- | :--------------------- | :------------------------------------------- |
+| COBOL |                        | COBOL SOURCE CODE                            |
+|       | \*NOPROC\*             | NO PROCESSORS ASSIGNED                       |
+|       | CLENBL **`(Default)`** | COBOL/LE COMPILE AND LINK, LISTING IS STORED |
+| COPY  |                        | COBOL COPYBOOK                               |
+|       | \*NOPROC\*             | NO PROCESSORS ASSIGNED                       |
--- test/TestHelper.ts
+++ test/TestHelper.ts
@@ -1,13 +1,21 @@
-export class TestHelper{
-    static getReportRecords(data: any): any{
-        return data.Record.map((a: { [x: string]: any; }) => a['ADOPTED-REPORT-REC']).map((rec: { [x: string]: any; }) => {return {dogBreed: rec['OUT-DOG-BREED'], adoptionAmount: rec['OUT-ADOPTED-AMOUNT']}});
-    }
+export class TestHelper {
+  static getReportRecords(data: any): any {
+    return data.Record.map(
+      (a: { [x: string]: any }) => a["ADOPTED-REPORT-REC"]
+    ).map((rec: { [x: string]: any }) => {
+      return {
+        dogBreed: rec["OUT-DOG-BREED"],
+        adoptionAmount: rec["OUT-ADOPTED-AMOUNT"],
+      };
+    });
+  }
 
-    static getAdoptionAmount(data: any, name: any): any{
-        let breed = data.filter((a: { dogBreed: string; }) => a.dogBreed == name)
-        if (!breed)
-            return undefined;
-        const adoptionAmounts = breed.map((a: { adoptionAmount: any; }) => a.adoptionAmount);
-        return adoptionAmounts.reduce((sum: any, a: any) => sum + a, 0);
-    }
+  static getAdoptionAmount(data: any, name: any): any {
+    let breed = data.filter((a: { dogBreed: string }) => a.dogBreed == name);
+    if (!breed) return undefined;
+    const adoptionAmounts = breed.map(
+      (a: { adoptionAmount: any }) => a.adoptionAmount
+    );
+    return adoptionAmounts.reduce((sum: any, a: any) => sum + a, 0);
+  }
 }
