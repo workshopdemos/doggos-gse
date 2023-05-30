801c3c028ca98773c0d81b4ba2a1ec42911df3c4
Sergei Kurnevich <sergei.kurnevich@broadcom.com>
2023-05-30 12:26:35 +0200

    renamed files to be jcl

--- scripts/deploy-ds.sh
+++ scripts/deploy-ds.sh
@@ -13,12 +13,12 @@ DBGDOG="DBGDOG$formatted_value"
 
 mkdir "$LOCAL_DIR/tmp"
 
-sed "s|<USERN>|$USERN|g" "$LOCAL_DIR/jcl/DBGDOG.txt" > "$LOCAL_DIR/tmp/DBGDOG.txt"
-sed -i "s|<DBGDOG>|$DBGDOG|g" "$LOCAL_DIR/tmp/DBGDOG.txt"
+sed "s|<USERN>|$USERN|g" "$LOCAL_DIR/jcl/DBGDOG.jcl" > "$LOCAL_DIR/tmp/DBGDOG.jcl"
+sed -i "s|<DBGDOG>|$DBGDOG|g" "$LOCAL_DIR/tmp/DBGDOG.jcl"
 
-sed "s|<LSTPRF>|$LSTPRF|g" "$LOCAL_DIR/jcl/LSTPRF.txt" > "$LOCAL_DIR/tmp/LSTPRF.txt"
+sed "s|<LSTPRF>|$LSTPRF|g" "$LOCAL_DIR/jcl/LSTPRF.jcl" > "$LOCAL_DIR/tmp/LSTPRF.jcl"
 
-sed "s|<RUNDOG>|$RUNDOG|g" "$LOCAL_DIR/jcl/RUNDOG.txt" > "$LOCAL_DIR/tmp/RUNDOG.txt"
+sed "s|<RUNDOG>|$RUNDOG|g" "$LOCAL_DIR/jcl/RUNDOG.jcl" > "$LOCAL_DIR/tmp/RUNDOG.jcl"
 
 zowe files delete data-set "$USERN.PUBLIC.JCL" -f
 zowe files delete data-set "$USERN.PUBLIC.INPUT" -f
