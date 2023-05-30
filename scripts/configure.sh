2becc3bffe1e02c3602105f1824fe3ae13bd32e3
Vit Gottwald <vit.gottwald@broadcom.com>
2023-05-30 12:43:04 +0200

    remove gse 2022 endeovor files

--- scripts/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-printf "\nConfiguring Test4z...\n\n"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.hlq "$1"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.user "$1"
-npx zowe config set profiles.lpar1.profiles.test4z.properties.password "$1"
-npx zowe config set profiles.lpar1.profiles.zosmf.properties.user "$1"
-npx zowe config set profiles.lpar1.profiles.zosmf.properties.password "$1"
-printf "\n#### Test4z configuration successful ####"
